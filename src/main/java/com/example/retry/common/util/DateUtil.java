cb8b95346075d51bab69027377519a838c27a9d3
liyongyong <liyongyong@fofund.com.cn>
2021-06-28 12:32:01 +0800

    增加log format

---
+++ src/main/java/com/example/retry/common/util/DateUtil.java
@@ -0,0 +1,16 @@
+package com.example.retry.common.util;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author 李勇勇
+ * @version 1.0
+ * @date 2021-06-28 10:31
+ */
+@Slf4j
+public final class DateUtil {
+
+
+}
---
+++ src/main/java/com/example/retry/common/util/LogUtil.java
@@ -0,0 +1,27 @@
+package com.example.retry.common.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author 李勇勇
+ * @version 1.0
+ * @date 2021-06-28 11:22
+ */
+@Slf4j
+public class LogUtil {
+
+
+    /**
+     * URL:http://www.baidu.com,
+     * METHOD:createPerson,
+     * ARGS:aaaa
+     * @param args
+     */
+    public static void main(String[] args) {
+        String format = "\nURL:{},\nMETHOD:{},\nARGS:{}";
+        log.info(format,"http://www.baidu.com","createPerson","aaaa");
+    }
+
+
+
+}
